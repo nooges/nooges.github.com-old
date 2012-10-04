9071c0b95986f2b5a403396c059cad013c3ed02d
Danny Nguyen <nooges@gmail.com>
2012-10-04 09:56:36 -0400

    Fixed crappy non-printing characters

--- quote.js
+++ quote.js
@@ -3,58 +3,58 @@ var tid;
 
 function quoteStr(last, change, time)
 {
-  switch (change.charAt(0))
-  {
-    case '+':
-      fontColor = 'green';
-      break;
-    case '-':
-      fontColor = 'red';
-      break;
-    default:
-      fontColor = 'black';
-  }
-
-  percentChange = Math.round(10000*+change/+last)/100;
-  output = last + " <b>Change:</b> <font color=" + fontColor + ">";
-  output += change + " (" + percentChange + "%)</font>";
-  output += " (" + time + ")";
-  return output;
+switch (change.charAt(0))
+{
+  case '+':
+    fontColor = 'green';
+    break;
+  case '-':
+    fontColor = 'red';
+    break;
+  default:
+    fontColor = 'black';
 }
-          
+
+percentChange = Math.round(10000*+change/+last)/100;
+output = last + " <b>Change:</b> <font color=" + fontColor + ">";
+output += change + " (" + percentChange + "%)</font>";
+output += " (" + time + ")";
+return output;
+}
+    
 function printQuote()
 {
-  var d = new Date();
-  var n = d.toTimeString();
-  var ticker = 'AAPL';
-  var url = 'http://www.google.com/finance/info?infotype=infoquoteall&q=' + ticker + '&callback=?';
-  $.getJSON(url, function(data)
-  {
-    var items = [];
-    $.each(data, function(key, datum)
-    {
-      items.push("<center><table border=1><tr><td><font size=3><center>");
-      output = quoteStr(datum.l, datum.c, datum.lt);
-      items.push("<b>" + datum.name + " (" + datum.t + ")</b> " + output);
-      if (datum.hasOwnProperty("el"))
-      {
-        output = quoteStr(datum.el, datum.ec, datum.elt);
-        items.push("<br><b>Extended Hours:</b> " + output);
-      }
-      items.push("<br><b>Range:</b> " + datum.lo + '-' + datum.hi);
-      items.push(" <b>Volume/Average:</b> " + datum.vo + "/" + datum.avvo);
-      //items.push("<br>" + n);
-      items.push("</center></font></td></tr></table></center>");
-    });
-    $("#stockquote").html(items.join(""));
-  });
-  tid = setTimeout(printQuote, timeout);
+  var d = new Date();
+  var n = d.toTimeString();
+  var ticker = 'AAPL';
+  var url = 'http://www.google.com/finance/info?infotype=infoquoteall&q=' + ticker + '&callback=?';
+  $.getJSON(url, function(data)
+  {
+    var items = [];
+    $.each(data, function(key, datum)
+    {
+      items.push("<center><table border=1><tr><td><font size=3><center>");
+      output = quoteStr(datum.l, datum.c, datum.lt);
+      items.push("<b>" + datum.name + " (" + datum.t + ")</b> " + output);
+      if (datum.hasOwnProperty("el"))
+      {
+         output = quoteStr(datum.el, datum.ec, datum.elt);
+         items.push("<br><b>Extended Hours:</b> " + output);
+      }
+      items.push("<br><b>Range:</b> " + datum.lo + '-' + datum.hi);
+      items.push(" <b>Volume/Average:</b> " + datum.vo + "/" + datum.avvo);
+      //items.push("<br>" + n);
+      items.push("</center></font></td></tr></table></center>");
+    });
+    $("#stockquote").html(items.join(""));
+  });
+  tid = setTimeout(printQuote, timeout);
 }
 
 // To be called when you want to stop the timer
 function abortTimer()
 {
-  clearInterval(tid);
+  clearInterval(tid);
 }
 
 printQuote();
